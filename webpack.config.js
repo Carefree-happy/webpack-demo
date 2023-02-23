66a7f90fdf1831fcc0f27b6f12921edeee158df3
Carefree-happy <2507057523@qq.com>
2023-02-23 23:56:41 +0800

    ci: 防止重复入口依赖

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
+    mode: "development",
     entry: {
-        index: "./src/index.js",
-        another: "./src/another-module.js",
+        index: {
+            import: "./src/index.js",
+            dependOn: "shared",
+        },
+        another: {
+            import: "./src/another-module.js",
+            dependOn: "shared",
+        },
+        shared: "lodash",
     },
-    devtool: 'inline-source-map',
+
+    devtool: "inline-source-map",
     devServer: {
-        static: './dist'
+        static: "./dist",
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development',
-        })
+            title: "Development",
+        }),
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
-        clean: true
+        clean: true,
     },
     optimization: {
-        runtimeChunk: 'single'
-    }
+        runtimeChunk: "single",
+    },
 };
